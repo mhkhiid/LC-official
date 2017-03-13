91bec97a33dfe56139aca41264dee2f840f0513a
mhkhiid <dinghengde@gmail.com>
2017-03-13 14:24:45 -0700

    Bug Recreation

--- run_linear.sh
+++ run_linear.sh
@@ -2,13 +2,13 @@
 
 python run.py \
 --action train \
---data-file data_train.csv \
+--data-file data_train_sample.csv \
 --param-file params.linear \
 --exp-dir exp/linear_7f
 echo "Start testing model"
 python run.py \
 --action test \
---data-file data_dev.csv \
+--data-file data_dev_sample.csv \
 --exp-dir exp/linear_7f
 echo "Testing finished"
 bash
